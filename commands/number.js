46c248cc8d356ab9c6ad6fc5dfae1b9a24550486
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-01-25 08:51:24 -0700

    Update number.js

--- commands/number.js
+++ commands/number.js
@@ -1,41 +1,24 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 module.exports = {
-	name: 'downtime',
+	name: 'number',
 	description: '||NOT FOR PUBLIC USE; CREATOR ONLY COMMAND|| N/A',
 	execute(message) {
-		function fetch() {
-			fetch('numbers.json')
-				.then(function(response) {
-					return response.json();
-				})
-				.then(function(data) {
-					appendData(data);
-				})
-				.catch(function(err) {
-					console.log(err);
-				});
-			function appendData(data) {
-				const document = 'C:/Users/mjsey/Downloads/httpd-2.4.46-o111i-x64-vc15/Apache24/htdocs/customization.html';
-				const mainContainer = document.getElementById('selected');
-				for (let i = 0; i < data.length; i++) {
-					const option = document.createElement('option');
-					const att = document.createAttribute('value');
-					const att2 = document.createAttribute('label');
-					option.innerHTML = data[i].number;
-					att.value = data[i].number;
-					att2.value = 'Selected Number\'s ID: ' + data[i].id;
-					mainContainer.appendChild(option);
-					option.setAttributeNode(att);
-					option.setAttributeNode(att2);
-				}
-			}
+	        var data_from_ajax;
+
+                $.get('https://minerbot.xyz/output.php', function(data) {
+                  data_from_ajax = data;
+                });
+		function user() {
+			var data_from_ajax2;
+			$.get('https://minerbot.xyz/index.php', function(data) {
+				data_from_ajax2 = data;
+			});
 		}
 		if (message.author.id === '444767734409068545') {
-			const user = fetch();
-			user.send('');
-			message.delete({ timeout: 500 })
-				.then(msg => console.log(`Entering Downtime now ($); called by you, ${msg.author.username}`))
-				.catch(console.error);
+			const user = user();
+			const numbers = data_from_ajax;
+			message.guild.channels.find(m
+			user.send(`${numbers`);
 		}
 	},
 };

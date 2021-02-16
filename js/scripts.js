2cea5772e62064831dcf28c36ea51fa8d2245579
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-02-16 11:06:44 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -50,14 +50,14 @@ const btn = document.querySelector('.btn-toggle');
 btn.addEventListener('click', function() {
   // Then toggle (add/remove) the .dark-theme class to the body
   document.body.classList.toggle('dark-theme');
-  $("#SearchQuery").classList.toggle("dark-theme");
-  $("#mainNav").classList.toggle('dark-theme');
+  document.input.$("#SearchQuery").classList.toggle("dark-theme");
+  document.nav.$("#mainNav").classList.toggle('dark-theme');
 //  if ($("#mainNav").classList.includes("navbar-dark")) {
 //     $("mainNav").removeClass("navbar-dark").addClass("navbar-light");
 //  } else {
 //     $("mainNav").removeClass("navbar-light").addClass("navbar-dark");
 //  };
-//})
+})
   // Magnific popup calls
   $('#portfolio').magnificPopup({
     delegate: 'a',

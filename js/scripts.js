e5ef5c48cf64df4fddcf09a2219a4931fd30642e
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-02-16 08:30:13 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -43,7 +43,14 @@
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
+// Select the button
+const btn = document.querySelector('.btn-toggle');
 
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');  
+})
   // Magnific popup calls
   $('#portfolio').magnificPopup({
     delegate: 'a',

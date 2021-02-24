f0fae6a04dd8bcae65917eb3c7769b8d450e0ad9
MJ Seybold <50940756+IcicleSavage@users.noreply.github.com>
2021-02-24 11:57:49 -0700

    Update credits.js

--- commands/credits.js
+++ commands/credits.js
@@ -11,7 +11,6 @@ module.exports = {
 			.setAuthor('IcicleSavage#0381', 'https://images-ext-2.discordapp.net/external/gZkTLgJMqUS-BIzhGtdQvfltRKPcLAijn7jBWhiUhus/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/444767734409068545/a_e32ddae0b3fbc2478616697c30e1f449.gif')
 			.addFields(
 				{ name: '**Created By: IcicleSavage||[#0381]||** \n Developed By: IcicleSavage||[#0381]||\n All Art Created By: IcicleSavage||[#0381]||', value: '||Script type used: Discord.js||\n Check out my **NEW** website: [here](https://minerbot.xyz)', inline: true },
-				{ name: '\u200b', value: logo['Mlogo'], inline: false },
 			);
 		message.channel.send(Credits);
 	},

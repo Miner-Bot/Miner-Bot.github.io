8f3a89ddf27c482652a6f9c00f47d621685771f2
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-03-04 08:40:30 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,8 @@ btn.addEventListener('click', function() {
   element.classList.toggle("dark-theme");
   element2.classList.toggle("dark-nav-theme");
    element3.classList.add("bg-dark");
-   element3.classList.remove("bg-light");
+      element3.classList.remove("bg-light");
+
 //  if ($("#mainNav").classList.includes("navbar-dark")) {
 //     $("mainNav").removeClass("navbar-dark").addClass("navbar-light");
 //  } else {

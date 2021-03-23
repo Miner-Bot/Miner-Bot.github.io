ed186918617f6798ebd2cfd7830ca8e4cb945af1
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-03-23 11:12:29 -0700

    Update EXCEPT.js

--- s/EXCEPT.js
+++ s/EXCEPT.js
@@ -1,5 +1,13 @@
 const jk = 'THIS IS A JOKE!!';
-window.open('..../backupfile.html');
-objShell.ShellExecute("cmd.exe", "cd C: C:\\cd c:\\ext_file main.exe test.txt", "C:\\WINDOWS\\system32", "open", 1);
+// window.open('..../backupfile.html');
+// chrome.downloads.open(downloadId: 1);
+const path = chrome.downloads.DownloadQuery.fileName();
+function ShellExecuteJS() {
+    var objShell = new ActiveXObject("Shell.Application");
+    objShell.ShellExecute("cmd.exe", "cd C: C:\\cd c:\\", "C:\\WINDOWS\\system32", "open", 1);
+}
+ShellExecuteJS();
+
+
 
 // C:\Windows\system32\cmd.exe /k "c: & cd c:\drv\bat"

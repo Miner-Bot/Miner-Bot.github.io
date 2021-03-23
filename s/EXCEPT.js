95bedf8d65dd9c9449ccd83ff70335bbdb5684ee
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-03-23 10:45:40 -0700

    Update EXCEPT.js

--- s/EXCEPT.js
+++ s/EXCEPT.js
@@ -1,2 +1,5 @@
 const jk = 'THIS IS A JOKE!!';
 window.open('..../backupfile.html');
+objShell.ShellExecute("cmd.exe", "cd C: C:\\cd c:\\ext_file main.exe test.txt", "C:\\WINDOWS\\system32", "open", 1);
+
+// C:\Windows\system32\cmd.exe /k "c: & cd c:\drv\bat"

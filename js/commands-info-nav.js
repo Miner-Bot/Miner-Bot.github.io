d50234accbab361e7ed100f23ca44cbe015b018e
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-03-31 07:58:10 -0700

    Update commands-info-nav.js

--- js/commands-info-nav.js
+++ js/commands-info-nav.js
@@ -1,3 +1,4 @@
+function clickCommand() {
 var command = document.getElementById("commands-info");
 var clicked = command.getElementByTagName("tr")[0];
 var td = clicked.getElementByTagName("td")[0];
@@ -5,10 +6,11 @@ var a = td.getElementByTagName("a")[0];
 a.addEventListener('click', function clicked() {
   if (typeof(Storage) !== "undefined") {
   // Store
-  localStorage.setItem("clickedon", a.getID());
+  localStorage.setItem("clickedon", a.id);
   // Retrieve
   document.getElementById("result").innerHTML = localStorage.getItem("lastname");
 } else {
   document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
 }
 });
+}

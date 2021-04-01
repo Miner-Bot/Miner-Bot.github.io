7cbca6a8c8911faf6783f8245582a571970cc597
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-04-01 13:05:55 -0700

    Update commands-info-nav.js

--- js/commands-info-nav.js
+++ js/commands-info-nav.js
@@ -1,8 +1,9 @@
-var command = document.getElementById("commands-info").getElementByTagName("tr")[0].getElementByTagName("td")[0].getElementByTagName("a")[0].onclick = store();
+document.getElementById("commands-info").getElementByTagName("tr")[0].getElementByTagName("td")[0].getElementByTagName("a")[0].onclick = store();
+var command = document.getElementById("commands-info");
 var clicked = command.getElementByTagName("tr")[0];
 var td = clicked.getElementByTagName("td")[0];
 var a = td.getElementByTagName("a")[0];
-a.onclick = data();
+a.onclick = store();
 function store() {
   if (typeof(Storage) !== "undefined") {
   // Store

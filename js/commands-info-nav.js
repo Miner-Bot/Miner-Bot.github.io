35e95c40547a409bb965885d3af6a5ba4aecc221
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-04-01 12:49:44 -0700

    Update commands-info-nav.js

--- js/commands-info-nav.js
+++ js/commands-info-nav.js
@@ -1,24 +1,15 @@
-/* function clickCommand() {
-var command = document.getElementById("commands-info");
+var command = document.getElementById("commands-info").getElementByTagName("tr")[0].getElementByTagName("td")[0].getElementByTagName("a")[0].onclick = store();
 var clicked = command.getElementByTagName("tr")[0];
 var td = clicked.getElementByTagName("td")[0];
 var a = td.getElementByTagName("a")[0];
 a.onclick = data();
-function data() {
+function store() {
   if (typeof(Storage) !== "undefined") {
   // Store
+    localStorage.setItem("clickedon", data[o].name
   // Retrieve
   location.hash = localStorage.getItem("clickedon");
 } else {
   location.hash = '';
 }
 };
-}
-function testo() {
-  var h2 = document.getElementByTagName("h2");
-  h2.hidden = false;
-  
-  
-}
-
-*/

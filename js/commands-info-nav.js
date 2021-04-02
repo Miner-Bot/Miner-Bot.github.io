ef504996a7881e50a397eea239cb322f199ce9b5
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-04-02 06:27:41 -0700

    Update commands-info-nav.js

--- js/commands-info-nav.js
+++ js/commands-info-nav.js
@@ -9,8 +9,6 @@
                         console.log(err);
                     });
 			
-                    function appendData(data) {
-			    for (var o = 0; o < data.length; o++) {
 document.getElementById("commands-info").getElementByTagName("tr")[o].getElementByTagName("td")[o].getElementByTagName("a")[o].onclick = store();
 var command = document.getElementById("commands-info");
 var clicked = command.getElementByTagName("tr")[o];
@@ -18,12 +16,13 @@ var td = clicked.getElementByTagName("td")[o];
 var a = td.getElementByTagName("a")[o];
 a.onclick = store(data);
 function store(data) {
-  if (typeof(Storage) !== "undefined") {
+	for (var o = 0; o < data.length; o++) {
+	if (typeof(Storage) !== "undefined") {
   // Store
     localStorage.setItem("clickedon", data[o].name);
   // Retrieve
   location.hash = localStorage.getItem("clickedon");
 } else {
   location.hash = '';
-}
+}}
 };

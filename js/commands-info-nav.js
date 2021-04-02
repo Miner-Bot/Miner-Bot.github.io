fc4fcb603d69b9930548eabad3add98c222e0066
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-04-02 06:26:38 -0700

    Update commands-info-nav.js

--- js/commands-info-nav.js
+++ js/commands-info-nav.js
@@ -1,13 +1,26 @@
-document.getElementById("commands-info").getElementByTagName("tr")[0].getElementByTagName("td")[0].getElementByTagName("a")[0].onclick = store();
+		    fetch('commands.json')
+                        .then(function (response) {
+                        return response.json();
+                    })
+                        .then(function (data) {
+                        store(data);
+                    })
+                        .catch(function (err) {
+                        console.log(err);
+                    });
+			
+                    function appendData(data) {
+			    for (var o = 0; o < data.length; o++) {
+document.getElementById("commands-info").getElementByTagName("tr")[o].getElementByTagName("td")[o].getElementByTagName("a")[o].onclick = store();
 var command = document.getElementById("commands-info");
-var clicked = command.getElementByTagName("tr")[0];
-var td = clicked.getElementByTagName("td")[0];
-var a = td.getElementByTagName("a")[0];
-a.onclick = store();
-function store() {
+var clicked = command.getElementByTagName("tr")[o];
+var td = clicked.getElementByTagName("td")[o];
+var a = td.getElementByTagName("a")[o];
+a.onclick = store(data);
+function store(data) {
   if (typeof(Storage) !== "undefined") {
   // Store
-    localStorage.setItem("clickedon", data[o].name
+    localStorage.setItem("clickedon", data[o].name);
   // Retrieve
   location.hash = localStorage.getItem("clickedon");
 } else {

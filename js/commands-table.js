32c38c881d143b04b40a226770e4fa3beb7f9ae8
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-04-05 07:40:04 -0700

    Update commands-table.js

--- js/commands-table.js
+++ js/commands-table.js
@@ -48,8 +48,8 @@ fetch('commands.json')
                         att2.value = 'text-box bungee-body-text';
                         att3.value = 'divider-page-width-mod my-2';
                         att4.value = 'bungee-small-body-text';
-                        onclick.value = 'localStorage.setItem("clickedon", data[o].name);';
-                        href.value = '/command#' + localStorage.getItem("clickedon");
+                        onclick.value = 'localStorage.setItem("clickedon", ' + data[o].name + ');';
+                        href.value = '/command#' + data[o].name;
                         
                         table.appendChild(tr);
                         tr.setAttributeNode(att);

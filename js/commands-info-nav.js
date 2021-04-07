c7ba94e0a819a3d7de14ea19aad7017c6a89d765
dsfas0976 <46836426+dsfas0976@users.noreply.github.com>
2021-04-07 07:29:09 -0700

    Update commands-info-nav.js

--- js/commands-info-nav.js
+++ js/commands-info-nav.js
@@ -1,4 +1,4 @@
-/*		    fetch('commands.json')
+		    fetch('commands.json')
                         .then(function (response) {
                         return response.json();
                     })
@@ -13,12 +13,12 @@ var a = document.getElementById("commands-info");
 	for (var o = 0; o < data.length; o++) {
 var element = a.getElementByTagName("tr")[o].getElementByTagName("td")[o].getElementByTagName("a");
 		var commandname = this.event.innerHTML;
-		// localStorage.setItem("clickedon", commandname);
+		localStorage.setItem("clickedon", commandname);
 
 
 	});
 }
-
+/*
 document.addEventListener('click', function(e) {
     e = e || window.event;
     var target = e.target || element,

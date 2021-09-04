5b2a642890e763414a1955a2653687236337f726
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-09-04 12:39:59 -0700

    Create numbersAppend.js

---
+++ js/numbersAppend.js
@@ -0,0 +1,24 @@
+fetch('numbers.json')
+  .then(function (response) {
+  return response.json();
+})
+  .then(function (data) {
+  appendData(data);
+})
+  .catch(function (err) {
+  console.log(err);
+});
+function appendData(data) {
+  var mainContainer = document.getElementById("selected");
+  for (var i = 0; i < data.length; i++) {
+    var option = document.createElement("option");
+    var att = document.createAttribute("value");
+    var att2 = document.createAttribute("label");
+    option.innerHTML = data[i].number;
+    att.value = data[i].number;
+    att2.value = 'This Number\'s ID: ' + data[i].id;
+    mainContainer.appendChild(option);
+    option.setAttributeNode(att);
+    option.setAttributeNode(att2);
+  }
+}

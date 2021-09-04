f315edd07eb1977445d3aa124b8b7589812bddbc
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-09-04 12:23:37 -0700

    Create numberTicket.js

---
+++ js/numberTicket.js
@@ -0,0 +1,41 @@
+if (window.location.search) {
+	const paramsString = window.location.search;
+	const searchParams = new URLSearchParams(paramsString);
+	const parent = document.getElementById('dataIn');
+	const date = new Date(searchParams.get('dateSubmitted').slice(11));
+	parent.innerHTML = 'Submitted on: ' + date.toDateString() + '<br>' + '=============================' + '<br>' + 'Number Information:';
+	for(var value of searchParams.values()) {
+		const div = document.createElement('div');
+		const att = document.createAttribute('class');
+		att.value = 'card-body';
+		div.setAttributeNode(att);
+		div.innerHTML = value;
+		parent.appendChild(div);
+	}
+}
+function getCookie(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+function checkCookie() {
+	var user = getCookie("user");
+	var selected = getCookie("selected");
+	const date = new Date();
+	if (user != "") {
+		const parent = document.getElementById('dataIn');
+		const div = document.createElement('div');
+		div.innerHTML = selected + '<br>' + 'For Discord User:' + '<br>' + user;
+		parent.appendChild(div);
+	}
+}

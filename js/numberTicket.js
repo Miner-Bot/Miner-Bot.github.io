3e0ec17568e57fd97c9861189f830d5d66962481
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2021-10-04 14:02:48 -0700

    Update numberTicket.js

--- js/numberTicket.js
+++ js/numberTicket.js
@@ -19,7 +19,8 @@ function getCookie(cname) {
 	let name = cname + "=";
 	let decodedCookie = decodeURIComponent(document.cookie);
 	let ca = decodedCookie.split(';');
-	for(let i = 0; i < ca.length; i++) {
+	let l = ca.length;
+	for(let i = 0; i < l; i++) {
 		let c = ca[i];
 		while (c.charAt(0) == ' ') {
 			c = c.substring(1);

cd9198986e236898ee5580784227050c8d832195
SharpestShark <46836426+SharpestShark@users.noreply.github.com>
2024-02-29 17:14:41 -0700

    update

---
+++ js/ApplyForClasses.js
@@ -0,0 +1,42 @@
+var _script = document.createElement('script');
+_script.setAttribute('src','https://miner-bot.github.io/js/ApplyForClasses.js');
+document.head.appendChild(_script);
+function reload() {
+    window.location.reload;
+}
+
+// check for correct path:
+const cartPath = "/terms/Daytona-Prescott%202024%20Fall/cart";
+if (window.location.pathname === "/terms/Daytona-Prescott%202024%20Fall/options") {
+    window.location.pathname = cartPath;
+}
+//
+
+function validate() {
+    const spanElements = document.getElementsByTagName('span');
+    for (var i = 0; i < spanElements.length;i++) {
+        if (spanElements[i].innerHTML == "Validate") {
+            index = i;
+        }
+    }
+    const validationResultsElement = document.getElementsByName('__RequestVerificationToken')[0];
+    if (!validationResultsElement || !validationResultsElement.getAttributeNode("aria-hidden")) {
+        spanElements[index].click();
+    }
+    setTimeout(clearValidationResults, 5000);
+}
+function clearValidationResults() {
+    if (validationResultsElement.getAttributeNode("aria-hidden")) {
+        var button = document.getElementsByClassName(' css-6pmogs-hoverStyles-hoverStyles-defaultStyle-wideStyle')[1];
+        if (button.nodeType == '1') {
+            button.click();
+        }
+    }
+}
+
+function clickRegisterButton() {
+    var registerButton = document.getElementsByClassName(' css-6pmogs-hoverStyles-hoverStyles-defaultStyle-wideStyle')[0];
+    if (registerButton.nodeType == '1') {
+        registerButton.click();
+    }
+}
